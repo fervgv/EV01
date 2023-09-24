c898d8ccb312c6824a2c6b495796ba2306b5d406
dani <danie@LAPTOP-5IJF3OK1>
2023-09-24 01:00:14 -0300

    Initial commit

---
+++ app/src/main/java/cl/ipvg/com/ev01/Main3.java
@@ -0,0 +1,14 @@
+package cl.ipvg.com.ev01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
